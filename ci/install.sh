ab38b216fadc4b183421f1025d4046c234e8c8a6
Martin Durant <martin.durant@utoronto.ca>
2019-07-16 22:12:51 -0400

    second try at that

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Install conda
+sudo apt-get update && sudo apt-get install libxss1  # squash gcc imcompat
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -7,6 +8,6 @@ conda config --set always_yes yes --set changeps1 no
 conda update conda
 
 # Install dependencies
-conda create -n test -c conda-forge python=3.7 pip pytest=4.5 paramiko requests dask distributed libxss1
+conda create -n test -c conda-forge python=3.7 pip pytest=4.5 paramiko requests dask distributed
 source activate test
 pip install . --no-deps
