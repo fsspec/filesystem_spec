b6a4de52d7db47239a137a623b0aabd027054a62
Martin Durant <martin.durant@utoronto.ca>
2019-07-18 11:15:28 -0400

    Add zstd, snappy and lz4 compression files

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,6 @@ conda config --set always_yes yes --set changeps1 no
 conda update conda
 
 # Install dependencies
-conda create -n test -c conda-forge python=3.7 pip pytest paramiko requests
+conda create -n test -c conda-forge python=3.7 pip pytest paramiko requests zstandard python-snappy lz4
 source activate test
 pip install . --no-deps
