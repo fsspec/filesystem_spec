46e90df2ae821031c8d969ea2b63badc177b3fab
Martin Durant <martin.durant@utoronto.ca>
2019-07-24 16:45:19 -0400

    many times

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # install FUSE
-# sudo apt-get install libfuse-dev
+sudo apt-get install libfuse-dev
 
 # Install conda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -11,7 +11,7 @@ conda update conda
 
 # Install dependencies
 conda create -n test -c conda-forge python=3.7 pip pytest paramiko requests zstandard python-snappy lz4 distributed \
-    dask fusepy pyarrow  pyftpdlib cloudpickle pyarrow pytest-cov -y -c defaults -c conda-forge
-pip install hadoop-test-cluster==0.1.0 # fusepy
+    dask pyarrow  pyftpdlib cloudpickle pyarrow pytest-cov -y -c defaults -c conda-forge
+pip install hadoop-test-cluster==0.1.0 fusepy
 source activate test
 pip install . --no-deps
