fsspec/filesystem_spec

commit 4749ab9c5f786a6ce9cdc3a098c12f205e8cf207

Fixes #110

fsspec/core.py
@@ -476,7 +476,7 @@ class BytesCache(BaseCache):
             bend = min(self.size, end + self.blocksize)
         else:
             bend = end
-        if bend == start:
+        if bend == start or start > self.size:
             return b""
         if ((self.start is None or start < self.start) and
                 (self.end is None or end > self.end)):

fsspec/tests/test_file.py
@@ -146,3 +146,17 @@ def test_midread_cache(ftp_writable):
         size = 17562187
         d3 = f.read(size)
         assert len(d3) == size
+
+
+def test_read_block(ftp_writable):
+    # not the same as test_read_block in test_utils, this depends on the
+    # behaviour of the bytest caching
+    from fsspec.utils import read_block
+    host, port, user, pw = ftp_writable
+    fs = FTPFileSystem(host=host, port=port,
+                       username=user, password=pw)
+    fn = "/myfile"
+    with fs.open(fn, 'wb') as f:
+        f.write(b'a,b\n1,2')
+    f = fs.open(fn, 'rb', cache_type='bytes')
+    assert read_block(f, 0, 6400, b'\n') == b'a,b\n1,2'

commit 961b749bd1f7f752625ed6c83290513d09e1c46a

BUG: Fixed file fetching issue
Closes https://github.com/dask/s3fs/issues/225

Which apparently was broken by 4749ab9c5f786a6ce9cdc3a098c12f205e8cf207.

Still working on a test.

fsspec/core.py
@@ -498,7 +498,7 @@ class BytesCache(BaseCache):
                 self.cache = self.fetcher(start, bend)
                 self.start = start
             else:
-                new = self.fetcher(self.end, bend)
+                new = self.fetcher(self.end, max(end, bend))
                 self.cache = self.cache + new
 
         self.end = self.start + len(self.cache)