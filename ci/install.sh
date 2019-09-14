f71b1fb9972bbdc09e0745b63e83df66dd247ade
Alex Ford <a.sewall.ford@gmail.com>
2019-09-14 08:31:46 +0000

    Convert to tox-conda test environments.
    Convert test environment setup to tox via tox-conda. Add py35-37 test
    configurations.

--- ci/install.sh
+++ ci/install.sh
@@ -5,10 +5,3 @@ sudo apt-get install libfuse-dev
 
 # install conda
 source $(dirname $BASH_SOURCE)/install_conda.sh
-
-# Install dependencies
-conda create -n test -c conda-forge python=3.7 pip pytest paramiko requests zstandard python-snappy lz4 distributed \
-    dask pyarrow  pyftpdlib cloudpickle pyarrow pytest-cov -y -c defaults -c conda-forge
-pip install hadoop-test-cluster==0.1.0 fusepy
-source activate test
-pip install . --no-deps
