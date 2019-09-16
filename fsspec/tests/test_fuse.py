bb7f196da434b7d144f41e504ce24391f3c4e7fa
Alex Ford <a.sewall.ford@gmail.com>
2019-09-16 21:01:06 +0000

    Run test_fuse in subprocess, rather than thread.
    Resolve deadlock in test_fuse in tox/linux environment by hosting fuse
    component in subprocess, rather than thread of test process.

--- fsspec/tests/test_fuse.py
+++ fsspec/tests/test_fuse.py
@@ -1,79 +1,81 @@
+import logging
 import os
+import signal
+import time
+from multiprocessing import Process
+
 import pytest
+
+from fsspec.fuse import run
 from fsspec.implementations.memory import MemoryFileSystem
 
 pytest.importorskip("fuse")
-from fsspec.fuse import run
-import time
 
-import logging
+
 logger = logging.getLogger(__name__)
 
 
-def test_basic(tmpdir):
-    tmpdir = str(tmpdir)
+def host_fuse(mountdir):
     fs = MemoryFileSystem()
-    fs.touch('/mounted/testfile')
-    logger.debug(fs)
+    fs.touch("/mounted/testfile")
+    run(fs, "/mounted/", mountdir)
 
-    th = run(fs, '/mounted/', tmpdir, False)
-    logger.debug("run %s", tmpdir)
 
-    timeout = 10
-    while True:
-        try:
-            logger.debug("listdir")
-            # can fail with device not ready while waiting for fuse
-            if 'testfile' in os.listdir(tmpdir):
-                logger.debug("break")
-                break
-        except:
-            pass
-        timeout -= 1
-        time.sleep(1)
-        assert timeout > 0, "Timeout"
+def test_basic(tmpdir):
+    mountdir = str(tmpdir.mkdir("mount"))
 
-    logger.debug("write")
-    fn = os.path.join(tmpdir, 'test')
-    with open(fn, 'wb') as f:
-        f.write(b'data')
-    logger.debug("info")
-    assert fs.info("/mounted/test")['size'] == 4
+    fuse_process = Process(target=host_fuse, args=(str(mountdir),))
+    fuse_process.start()
+    try:
 
-    logger.debug("open")
-    f = open(fn)
+        logger.debug("run %s", mountdir)
 
-    logger.debug("read")
-    assert f.read() == "data"
+        timeout = 10
+        while True:
+            try:
+                logger.debug("listdir")
+                # can fail with device not ready while waiting for fuse
+                if "testfile" in os.listdir(mountdir):
+                    logger.debug("break")
+                    break
+            except:
+                pass
+            timeout -= 1
+            time.sleep(1)
+            assert timeout > 0, "Timeout"
 
-    logger.debug("remove")
-    os.remove(fn)
+        logger.debug("write")
+        fn = os.path.join(mountdir, "test")
+        with open(fn, "wb") as f:
+            f.write(b"data")
+        logger.debug("info")
+        # assert fs.info("/mounted/test")['size'] == 4
 
-    logger.debug("mkdir")
-    os.mkdir(fn)
+        logger.debug("open")
+        f = open(fn)
 
-    logger.debug("listdir")
-    assert os.listdir(fn) == []
+        logger.debug("read")
+        assert f.read() == "data"
 
-    logger.debug("mkdir")
-    os.mkdir(fn + '/inner')
+        logger.debug("remove")
+        os.remove(fn)
 
-    logger.debug("rmdir")
-    with pytest.raises(OSError):
+        logger.debug("mkdir")
+        os.mkdir(fn)
+
+        logger.debug("listdir")
+        assert os.listdir(fn) == []
+
+        logger.debug("mkdir")
+        os.mkdir(fn + "/inner")
+
+        logger.debug("rmdir")
+        with pytest.raises(OSError):
+            os.rmdir(fn)
+
+        os.rmdir(fn + "/inner")
         os.rmdir(fn)
-
-    os.rmdir(fn + "/inner")
-    os.rmdir(fn)
-    assert not fs.pseudo_dirs
-
-    # should not normally kill a thread like this, but FUSE blocks, so we
-    # cannot have thread listen for event. Alternative may be to .join() but
-    # send a SIGINT
-    logger.debug("release")
-    th._tstate_lock.release()
-    logger.debug("_stop")
-    th._stop()
-    logger.debug("join")
-    th.join()
-    logger.debug("clear")
-    fs.store.clear()
+        # assert not fs.pseudo_dirs
+    finally:
+        os.kill(fuse_process.pid, signal.SIGTERM)
+        fuse_process.join()
