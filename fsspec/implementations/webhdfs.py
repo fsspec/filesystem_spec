14ee670519e104672d383a2479767b82e10233cd
Airo, Sami <Sami.Airo@ecb.europa.eu>
2020-03-31 20:46:58 +0200

    Support HttpFS intricacies

--- fsspec/implementations/webhdfs.py
+++ fsspec/implementations/webhdfs.py
@@ -226,10 +226,14 @@ class WebHDFS(AbstractFileSystem):
     def ukey(self, path):
         """Checksum info of file, giving method and result"""
         out = self._call("GETFILECHECKSUM", path=path, redirect=False)
-        location = self._apply_proxy(out.headers["Location"])
-        out2 = self.session.get(location)
-        out2.raise_for_status()
-        return out2.json()["FileChecksum"]
+        if "Location" in out.headers:
+            location = self._apply_proxy(out.headers["Location"])
+            out2 = self.session.get(location)
+            out2.raise_for_status()
+            return out2.json()["FileChecksum"]
+        else:
+            out.raise_for_status()
+            return out.json()["FileChecksum"]
 
     def home_directory(self):
         """Get user's home directory"""
@@ -352,7 +356,11 @@ class WebHDFile(AbstractBufferedFile):
             This is the last block, so should complete file, if
             self.autocommit is True.
         """
-        out = self.fs.session.post(self.location, data=self.buffer.getvalue())
+        out = self.fs.session.post(
+            self.location,
+            data=self.buffer.getvalue(),
+            headers={"content-type": "application/octet-stream"},
+        )
         out.raise_for_status()
         return True
 
@@ -369,7 +377,9 @@ class WebHDFile(AbstractBufferedFile):
         location = self.fs._apply_proxy(out.headers["Location"])
         if "w" in self.mode:
             # create empty file to append to
-            out2 = self.fs.session.put(location)
+            out2 = self.fs.session.put(
+                location, headers={"content-type": "application/octet-stream"}
+            )
             out2.raise_for_status()
         self.location = location.replace("CREATE", "APPEND")
 
@@ -378,9 +388,12 @@ class WebHDFile(AbstractBufferedFile):
             "OPEN", path=self.path, offset=start, length=end - start, redirect=False
         )
         out.raise_for_status()
-        location = out.headers["Location"]
-        out2 = self.fs.session.get(self.fs._apply_proxy(location))
-        return out2.content
+        if "Location" in out.headers:
+            location = out.headers["Location"]
+            out2 = self.fs.session.get(self.fs._apply_proxy(location))
+            return out2.content
+        else:
+            return out.content
 
     def commit(self):
         self.fs.mv(self.path, self.target)
