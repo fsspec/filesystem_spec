9ebaf19a1496827cd270d49e0bc5c1351b2b033e
Patrick Sodré <psodre@gmail.com>
2020-06-26 10:46:45 -0400

    Show that chaining zip::zip::file works

--- fsspec/tests/test_api.py
+++ fsspec/tests/test_api.py
@@ -187,6 +187,31 @@ def test_multilevel_chained_fs():
             assert f.read().decode("utf-8") == f.name
 
 
+def test_multilevel_chained_fs_zip_zip_file():
+    """This test reproduces intake/filesystem_spec#334"""
+    import zipfile
+
+    d1 = tempfile.mkdtemp()
+    f1 = os.path.join(d1, "f1.zip")
+    f2 = os.path.join(d1, "f2.zip")
+    with zipfile.ZipFile(f1, mode="w") as z:
+        # filename, content
+        z.writestr("foo.txt", "foo.txt")
+        z.writestr("bar.txt", "bar.txt")
+
+    with zipfile.ZipFile(f2, mode="w") as z:
+        with open(f1, "rb") as f:
+            z.writestr("f1.zip", f.read())
+
+    # We expected this to be the correct syntax
+    of = fsspec.open_files(f"zip://*.txt::zip://f1.zip::file://{f2}")
+
+    assert len(of) == 2
+    for open_file in of:
+        with open_file as f:
+            assert f.read().decode("utf-8") == f.name
+
+
 def test_chained_equivalent():
     d1 = tempfile.mkdtemp()
     d2 = tempfile.mkdtemp()
