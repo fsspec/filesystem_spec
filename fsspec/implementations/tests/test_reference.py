cff13a90bfc747a6ae3271e6660d7e6c686b2be1
Martin Durant <martin.durant@utoronto.ca>
2020-11-04 09:45:32 -0500

    test ls

--- fsspec/implementations/tests/test_reference.py
+++ fsspec/implementations/tests/test_reference.py
@@ -17,12 +17,22 @@ def test_simple(server):  # noqa: F811
     assert fs.cat("c") == data[1:6]
 
 
+def test_ls(server):  # noqa: F811
+    refs = {"a": b"data", "b": (realfile, 0, 5), "c/d": (realfile, 1, 6)}
+    h = fsspec.filesystem("http")
+    fs = fsspec.filesystem("reference", references=refs, fs=h)
+
+    assert fs.ls("", detail=False) == ["a", "b", "c"]
+    assert fs.find("") == ["a", "b", "c/d"]
+    assert fs.find("", withdirs=True) == ["a", "b", "c", "c/d"]
+
+
 def test_err(m):
     with pytest.raises(NotImplementedError):
         fsspec.filesystem("reference", references={}, fs=m)
 
 
-data = """{
+jdata = """{
     "metadata": {
         ".zattrs": {
             "Conventions": "UGRID-0.9.0"
@@ -63,6 +73,6 @@ data = """{
 
 
 def test_unmodel():
-    refs = _unmodel_hdf5(json.loads(data))
+    refs = _unmodel_hdf5(json.loads(jdata))
     assert b'"Conventions": "UGRID-0.9.0"' in refs[".zattrs"]
     assert refs["adcirc_mesh/0"] == ("https://url", 8928, 8932)
