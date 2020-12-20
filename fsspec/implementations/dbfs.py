eaafcf233f1069ac65ea41e380b6826d9cbccf4d
Nils Braun <nilslennartbraun@gmail.com>
2020-12-20 22:37:43 +0100

    Refined dbfs implementation and tested it in a real cluster

--- fsspec/implementations/dbfs.py
+++ fsspec/implementations/dbfs.py
@@ -1,16 +1,18 @@
 import base64
+import os
 from fsspec import AbstractFileSystem
 from fsspec.spec import AbstractBufferedFile
 import requests
 
 
 class DatabricksFileSystem(AbstractFileSystem):
-
-    def __init__(self, token, instance, **kwargs):
-        self.token = token
+    def __init__(self, instance, token, **kwargs):
         self.instance = instance
+        self.token = token
+
         self.session = requests.Session()
         self.session.headers.update(self.header)
+
         super().__init__(**kwargs)
 
     @property
@@ -18,19 +20,30 @@ class DatabricksFileSystem(AbstractFileSystem):
         return {"Authorization": f"Bearer {self.token}"}
 
     @property
-    def url(self)
-        return f"https://{self.instance}/api/2.0/clusters/list"
+    def url(self):
+        return f"https://{self.instance}/api"
 
     def ls(self, path, detail=True):
         out = self._ls_from_cache(path)
         if not out:
-            r = self.session.get(self.url + "/2.0/dbfs/list", json={"path": path})
-            r.raise_for_status()
-            out = [{"name": o["path"],
-                    "type": ["directory", "file"][o["is_dir"]],
-                    "size": o["file_size"]}
-                   for o in r.json()["files"]]
+            try:
+                r = self.get_from_api("list", json={"path": path})
+            except requests.exceptions.HTTPError as e:
+                status_code = e.response.status_code
+                if status_code == 404:
+                    raise FileNotFoundError(path)
+                raise e
+            files = r["files"]
+            out = [
+                {
+                    "name": o["path"],
+                    "type": "directory" if o["is_dir"] else "file",
+                    "size": o["file_size"],
+                }
+                for o in files
+            ]
             self.dircache[path] = out
+
         if detail:
             return out
         return [o["name"] for o in out]
@@ -38,29 +51,65 @@ class DatabricksFileSystem(AbstractFileSystem):
     def mkdirs(self, path, exist_ok=True):
         if not exist_ok:
             raise NotImplementedError
-        r = self.session.post(self.url + "/2.0/dbfs/mkdirs", json={"path": path})
-        r.raise_for_status()
+        self.post_to_api("mkdirs", json={"path": path})
 
-    def makedir(self, path, create_parents=True, **kwargs):
+    def makedirs(self, path, create_parents=True, **kwargs):
         if not create_parents:
             raise NotImplementedError
-        self.mkdirs(path)
+        self.mkdirs(path, **kwargs)
 
     def rm(self, path, recursive=False):
-        r = self.session.post(self.url + "/2.0/dbfs/delete",
-                              json={"path": path, "recursive": recursive})
-        r.raise_for_status()
+        self.post_to_api("delete", json={"path": path, "recursive": recursive})
 
-    def mv(self, path1, path2, recursive=False, maxdepth=None, **kwargs):
+    def mv(self, path1, path2, recursive=False, maxdepth=None):
         if recursive:
             raise NotImplementedError
-        r = self.session.post(self.url + "/2.0/dbfs/move",
-                              json={"source_path": path1, "destination_path": path2})
+        if maxdepth:
+            raise NotImplementedError
+
+        self.post_to_api(
+            "move",
+            json={"source_path": path1, "destination_path": path2},
+        )
+
+    def create_handle(self, path, overwrite=True):
+        r = self.post_to_api("create", json={"path": path, "overwrite": overwrite})
+        return r["handle"]
+
+    def close_handle(self, handle):
+        self.post_to_api("close", json={"handle": handle})
+
+    def add_data(self, handle, data):
+        data = base64.b64encode(data).decode()
+        self.post_to_api(
+            "add-block",
+            json={"handle": handle, "data": data},
+        )
+
+    def get_data(self, path, start, end):
+        r = self.get_from_api(
+            "read",
+            json={"path": path, "offset": start, "length": end - start},
+        )
+        return base64.b64decode(r["data"])
+
+    def post_to_api(self, endpoint, json):
+        r = self.session.post(os.path.join(self.url, "2.0/dbfs", endpoint), json=json)
         r.raise_for_status()
+
+        return r.json()
+
+    def get_from_api(self, endpoint, json):
+        r = self.session.get(os.path.join(self.url, "2.0/dbfs", endpoint), json=json)
+        r.raise_for_status()
+
+        return r.json()
+
+    def _open(self, path, mode="rb", block_size="default", **kwargs):
+        return DBFile(self, path, mode=mode, block_size=block_size, **kwargs)
 
 
 class DBFile(AbstractBufferedFile):
-
     DEFAULT_BLOCK_SIZE = 1 * 2 ** 20  # only allowed block size
 
     def __init__(
@@ -74,10 +123,18 @@ class DBFile(AbstractBufferedFile):
         cache_options=None,
         **kwargs
     ):
-        super().__init__(fs,
+        if block_size is None or block_size == "default":
+            block_size = self.DEFAULT_BLOCK_SIZE
+
+        assert (
+            block_size == self.DEFAULT_BLOCK_SIZE
+        ), f"Only the default block size is allowed, not {block_size}"
+
+        super().__init__(
+            fs,
             path,
             mode=mode,
-            block_size=self.DEFAULT_BLOCK_SIZE,
+            block_size=block_size,
             autocommit=autocommit,
             cache_type=cache_type,
             cache_options=cache_options or {},
@@ -86,23 +143,17 @@ class DBFile(AbstractBufferedFile):
 
     # these methods could be standalone functions, if you want to go async
     def _initiate_upload(self):
-        r = self.fs.session.post(self.fs.url + "2.0/dbfs/create",
-                                 json={"path": self.path, "overwrite": True})
-        r.raise_for_status
-        self.handle = r.json["handle"]
+        self.handle = self.fs.create_handle(self.path)
+        print("Handle:", self.handle)
 
     def _upload_chunk(self, final=False):
-        # for data in buffer, in 1MB blocks
-        r = self.fs.session.post(self.fs.url + "2.0/dbfs/add-block",
-                                 json={"handle": self.path, "data": base64.b64encode(data)})
-        r.raise_for_status
+        self.buffer.seek(0)
+        # TODO: for data in buffer, in 1MB blocks
+        print("Writing data")
+        self.fs.add_data(handle=self.handle, data=self.buffer.getvalue())
         if final:
-            r = self.fs.session.post(self.fs.url + "2.0/dbfs/close",
-                                     json={"handle": self.path})
-            r.raise_for_status
+            print("Closing handle", self.handle)
+            self.fs.close_handle(handle=self.handle)
 
     def _fetch_range(self, start, end):
-        r = self.fs.session.get(self.fs.url + "2.0/dbfs/read",
-                                json={"path": self.path, "offset": start, "length": end - start})
-        r.raise_for_status
-        return base64.b64decode(r["data"])
+        return self.fs.get_data(path=self.path, start=start, end=end)
