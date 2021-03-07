aeacbc75c01ebb6905e63887831597156c4bc179
Andreas Motl <andreas.motl@panodata.org>
2021-03-08 00:54:21 +0100

    Streamline archive file testing
    This bundles all tests against archive file filesystems (zip, tar,
    libarchive) into a single test suite in order to make absolutely sure
    all of those will adhere to the same specification.

--- fsspec/implementations/tests/test_tar.py
+++ fsspec/implementations/tests/test_tar.py
@@ -1,147 +1,20 @@
 import os
-import pickle
-import tarfile
-import tempfile
-from contextlib import contextmanager
-from io import BytesIO
 
 import pytest
 
 import fsspec
 from fsspec.core import OpenFile
-
-
-@contextmanager
-def temptar(data={}, mode="w", suffix=".tar"):
-    f = tempfile.mkstemp(suffix=suffix)[1]
-    with tarfile.TarFile.open(f, mode=mode) as t:
-        touched = {}
-        for name, data in data.items():
-
-            # Create directory hierarchy.
-            # https://bugs.python.org/issue22208#msg225558
-            if "/" in name and name not in touched:
-                parts = os.path.dirname(name).split("/")
-                for index in range(1, len(parts) + 1):
-                    info = tarfile.TarInfo("/".join(parts[:index]))
-                    info.type = tarfile.DIRTYPE
-                    t.addfile(info)
-                touched[name] = True
-
-            # Add file content.
-            info = tarfile.TarInfo(name=name)
-            info.size = len(data)
-            t.addfile(info, BytesIO(data))
-    try:
-        yield f
-    finally:
-        try:
-            os.remove(f)
-        except (IOError, OSError):
-            pass
-
-
-data = {"a": b"", "b": b"hello", "deeply/nested/path": b"stuff"}
-
-
-def test_empty():
-    with temptar() as t:
-        fs = fsspec.filesystem("tar", fo=t)
-        assert fs.find("") == []
-        assert fs.find("", withdirs=True) == []
-        with pytest.raises(FileNotFoundError):
-            fs.info("")
-        assert fs.ls("") == []
-
-
-def test_glob():
-    with temptar(data) as t:
-        fs = fsspec.filesystem("tar", fo=t)
-        assert fs.glob("*/*/*th") == ["deeply/nested/path"]
-
-
-def test_mapping():
-    with temptar(data) as t:
-        fs = fsspec.filesystem("tar", fo=t)
-        m = fs.get_mapper("")
-        assert list(m) == ["a", "b", "deeply/nested/path"]
-        assert m["b"] == data["b"]
-
-
-def test_pickle():
-    with temptar(data) as t:
-        fs = fsspec.filesystem("tar", fo=t)
-        fs2 = pickle.loads(pickle.dumps(fs))
-        assert fs2.cat("b") == b"hello"
-
-
-def test_all_dirnames():
-    with temptar() as t:
-        fs = fsspec.filesystem("tar", fo=t)
-
-        # fx are files, dx are a directories
-        assert fs._all_dirnames([]) == set()
-        assert fs._all_dirnames(["f1"]) == set()
-        assert fs._all_dirnames(["f1", "f2"]) == set()
-        assert fs._all_dirnames(["f1", "f2", "d1/f1"]) == {"d1"}
-        assert fs._all_dirnames(["f1", "d1/f1", "d1/f2"]) == {"d1"}
-        assert fs._all_dirnames(["f1", "d1/f1", "d2/f1"]) == {"d1", "d2"}
-        assert fs._all_dirnames(["d1/d1/d1/f1"]) == {"d1", "d1/d1", "d1/d1/d1"}
-
-
-def test_ls():
-    with temptar(data) as t:
-        lhs = fsspec.filesystem("tar", fo=t)
-
-        assert lhs.ls("") == ["a", "b", "deeply/"]
-        assert lhs.ls("/") == lhs.ls("")
-
-        assert lhs.ls("deeply") == ["deeply/nested/"]
-        assert lhs.ls("deeply/") == lhs.ls("deeply")
-
-        assert lhs.ls("deeply/nested") == ["deeply/nested/path"]
-        assert lhs.ls("deeply/nested/") == lhs.ls("deeply/nested")
-
-
-def test_find():
-    with temptar(data) as t:
-        lhs = fsspec.filesystem("tar", fo=t)
-
-        assert lhs.find("") == ["a", "b", "deeply/nested/path"]
-        assert lhs.find("", withdirs=True) == [
-            "a",
-            "b",
-            "deeply/",
-            "deeply/nested/",
-            "deeply/nested/path",
-        ]
-
-        assert lhs.find("deeply") == ["deeply/nested/path"]
-        assert lhs.find("deeply/") == lhs.find("deeply")
-
-
-def test_walk():
-    with temptar(data) as t:
-        fs = fsspec.filesystem("tar", fo=t)
-        expected = [
-            # (dirname, list of subdirs, list of files)
-            ("", ["deeply"], ["a", "b"]),
-            ("deeply", ["nested"], []),
-            ("deeply/nested", [], ["path"]),
-        ]
-        assert list(fs.walk("")) == expected
+from fsspec.implementations.tests.test_archive import archive_data, temptar
 
 
 def test_info():
-    with temptar(data) as t:
-        fs_cache = fsspec.filesystem("tar", fo=t)
+    with temptar(archive_data) as t:
+        fs = fsspec.filesystem("tar", fo=t)
 
-        with pytest.raises(FileNotFoundError):
-            fs_cache.info("i-do-not-exist")
-
-        # Iterate over all directories
-        for d in fs_cache._all_dirnames(data.keys()):
-            lhs = fs_cache.info(d)
+        # Iterate over all directories.
+        # Probe specific fields of Tar archives.
+        for d in fs._all_dirnames(archive_data.keys()):
+            lhs = fs.info(d)
             del lhs["chksum"]
             expected = {
                 "name": f"{d}/",
@@ -149,48 +22,26 @@ def test_info():
                 "type": "directory",
                 "devmajor": 0,
                 "devminor": 0,
-                "gid": 0,
                 "gname": "",
                 "linkname": "",
+                "uid": 0,
+                "gid": 0,
                 "mode": 420,
                 "mtime": 0,
-                "uid": 0,
                 "uname": "",
             }
             assert lhs == expected
 
-        # Iterate over all files
-        for f, v in data.items():
-            lhs = fs_cache.info(f)
-            assert lhs["name"] == f
-            assert lhs["size"] == len(v)
-            assert lhs["type"] == "file"
+        # Iterate over all files.
+        for f, v in archive_data.items():
+            lhs = fs.info(f)
 
-            # There are some flags specific to Tar files.
-            # These are two we can use to check we are getting some of them
+            # Probe some specific fields of Tar archives.
+            assert "mode" in lhs
+            assert "uid" in lhs
+            assert "gid" in lhs
+            assert "mtime" in lhs
             assert "chksum" in lhs
-
-
-@pytest.mark.parametrize("scale", [128, 512, 4096])
-def test_isdir_isfile(scale):
-    def make_nested_dir(i):
-        x = f"{i}"
-        table = x.maketrans("0123456789", "ABCDEFGHIJ")
-        return "/".join(x.translate(table))
-
-    scaled_data = {f"{make_nested_dir(i)}/{i}": b"" for i in range(1, scale + 1)}
-    with temptar(scaled_data) as t:
-        fs = fsspec.filesystem("tar", fo=t)
-
-        lhs_dirs, lhs_files = fs._all_dirnames(scaled_data.keys()), scaled_data.keys()
-
-        # Warm-up the Cache, this is done in both cases anyways...
-        fs._get_dirs()
-
-        entries = lhs_files | lhs_dirs
-
-        assert lhs_dirs == {e for e in entries if fs.isdir(e)}
-        assert lhs_files == {e for e in entries if fs.isfile(e)}
 
 
 @pytest.mark.parametrize(
@@ -201,12 +52,13 @@ def test_isdir_isfile(scale):
         {"mode": "w:bz2", "suffix": ".tar.bz2"},
         {"mode": "w:xz", "suffix": ".tar.xz"},
     ],
+    ids=["tar", "tar-gz", "tar-bz2", "tar-xz"],
 )
 def test_compressions(recipe):
     """
     Run tests on all available tar file compression variants.
     """
-    with temptar(data, mode=recipe["mode"], suffix=recipe["suffix"]) as t:
+    with temptar(archive_data, mode=recipe["mode"], suffix=recipe["suffix"]) as t:
         fs = fsspec.filesystem("tar", fo=t)
         assert fs.cat("b") == b"hello"
 
@@ -219,11 +71,12 @@ def test_compressions(recipe):
         {"mode": "w:bz2", "suffix": ".tar.bz2"},
         {"mode": "w:xz", "suffix": ".tar.xz"},
     ],
+    ids=["tar", "tar-gz", "tar-bz2", "tar-xz"],
 )
 def test_filesystem(recipe, tmpdir):
     """
     Run tests through a real fsspec filesystem implementation.
-    Here: ``LocalFileSystem``.
+    Here: `LocalFileSystem`.
     """
 
     filename = os.path.join(tmpdir, f'temp{recipe["suffix"]}')
@@ -231,7 +84,7 @@ def test_filesystem(recipe, tmpdir):
     fs = fsspec.filesystem("file")
     f = OpenFile(fs, filename, mode="wb")
 
-    with temptar(data, mode=recipe["mode"], suffix=recipe["suffix"]) as tf:
+    with temptar(archive_data, mode=recipe["mode"], suffix=recipe["suffix"]) as tf:
         with f as fo:
             fo.write(open(tf, "rb").read())
 
