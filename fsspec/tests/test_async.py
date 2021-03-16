ffb0d1fa022e2bcf8e1c507f8d78fb604611ec2d
Martin Durant <martin.durant@utoronto.ca>
2021-03-16 12:33:49 -0400

    fix async test

--- fsspec/tests/test_async.py
+++ fsspec/tests/test_async.py
@@ -11,14 +11,14 @@ async def inner():
     return True
 
 
-async def outer():
+async def outer(loop):
     await asyncio.sleep(1)
-    return _run_until_done(inner())
+    return _run_until_done(loop, inner())
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="Async fails on py36")
 def test_runtildone():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    assert loop.run_until_complete(outer())
+    assert loop.run_until_complete(outer(loop))
     loop.close()
