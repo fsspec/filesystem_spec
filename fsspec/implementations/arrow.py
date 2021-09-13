a384fb2c4248f22571a79981ab38fb4319023896
Batuhan Taskaya <batuhan@python.org>
2021-09-13 18:26:48 +0300

    arrow: implement ArrowFile, mirror_from, and exception management

--- fsspec/implementations/arrow.py
+++ fsspec/implementations/arrow.py
@@ -1,7 +1,32 @@
-from contextlib import closing
+import errno
+import io
+import os
+import secrets
+import shutil
+from contextlib import suppress
+from functools import wraps
 
 from fsspec import AbstractFileSystem
-from fsspec.utils import stringify_path
+from fsspec.utils import mirror_from, stringify_path
+
+
+def wrap_exceptions(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except OSError as exception:
+            if not exception.args:
+                raise
+
+            message, *args = exception.args
+            if "file does not exist" in message:
+                _, _, path = message.partition(": ")
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+            else:
+                raise
+
+    return wrapper
 
 
 class ArrowFSWrapper(AbstractFileSystem):
@@ -31,14 +56,17 @@ class ArrowFSWrapper(AbstractFileSystem):
     @classmethod
     def _strip_protocol(cls, path):
         path = stringify_path(path)
-        _, _, path = path.partition("://")
+        if "://" in path:
+            _, _, path = path.partition("://")
+
         return path
 
     def ls(self, path, detail=False, **kwargs):
         from pyarrow.fs import FileSelector
 
         entries = [
-            self._make_entry(entry) for entry in self.get_file_info(FileSelector(path))
+            self._make_entry(entry)
+            for entry in self.fs.get_file_info(FileSelector(path))
         ]
         if detail:
             return entries
@@ -57,6 +85,8 @@ class ArrowFSWrapper(AbstractFileSystem):
             kind = "directory"
         elif info.type is FileType.File:
             kind = "file"
+        elif info.type is FileType.NotFound:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), info.path)
         else:
             kind = "other"
 
@@ -67,26 +97,34 @@ class ArrowFSWrapper(AbstractFileSystem):
             "mtime": info.mtime,
         }
 
+    @wrap_exceptions
     def cp_file(self, path1, path2, **kwargs):
         path1 = self._strip_protocol(path1).rstrip("/")
         path2 = self._strip_protocol(path2).rstrip("/")
-        self.fs.copy_file(path1, path2)
 
-    def get_file(self, path1, path2, **kwargs):
-        return self.cp_file(path1, path2, **kwargs)
+        with self._open(path1, "rb") as lstream:
+            tmp_fname = "/".join([self._parent(path2), f".tmp.{secrets.token_hex(16)}"])
+            try:
+                with self.open(tmp_fname, "wb") as rstream:
+                    shutil.copyfileobj(lstream, rstream)
+                self.fs.move(tmp_fname, path2)
+            except BaseException:  # noqa
+                with suppress(FileNotFoundError):
+                    self.fs.delete_file(tmp_fname)
+                raise
 
-    def put_file(self, path1, path2, **kwargs):
-        return self.cp_file(path1, path2, **kwargs)
-
+    @wrap_exceptions
     def mv_file(self, path1, path2, **kwargs):
         path1 = self._strip_protocol(path1).rstrip("/")
         path2 = self._strip_protocol(path2).rstrip("/")
         self.fs.move(path1, path2)
 
+    @wrap_exceptions
     def rm_file(self, path):
         path = self._strip_protocol(path)
         self.fs.delete_file(path)
 
+    @wrap_exceptions
     def rm(self, path, recursive=False, maxdepth=None):
         path = self._strip_protocol(path).rstrip("/")
         if recursive and self.isdir(path):
@@ -94,6 +132,7 @@ class ArrowFSWrapper(AbstractFileSystem):
         else:
             self.fs.delete_file(path)
 
+    @wrap_exceptions
     def _open(self, path, mode="rb", block_size=None, **kwargs):
         if mode == "rb":
             stream = self.fs.open_input_stream(path)
@@ -102,8 +141,9 @@ class ArrowFSWrapper(AbstractFileSystem):
         else:
             raise ValueError(f"unsupported mode for Arrow filesystem: {mode!r}")
 
-        return closing(stream)
+        return ArrowFile(self, stream, path, mode, block_size, **kwargs)
 
+    @wrap_exceptions
     def mkdir(self, path, create_parents=True, **kwargs):
         path = self._strip_protocol(path)
         if create_parents:
@@ -111,10 +151,58 @@ class ArrowFSWrapper(AbstractFileSystem):
         else:
             self.fs.create_dir(path, recursive=False)
 
+    @wrap_exceptions
     def makedirs(self, path, exist_ok=False):
         path = self._strip_protocol(path)
         self.fs.create_dir(path, recursive=True)
 
+    @wrap_exceptions
     def rmdir(self, path):
         path = self._strip_protocol(path)
         self.fs.delete_dir(path)
+
+
+@mirror_from(
+    "stream", ["read", "seek", "tell", "write", "readable", "writable", "close"]
+)
+class ArrowFile(io.IOBase):
+    def __init__(self, fs, stream, path, mode, block_size=None, **kwargs):
+        self.path = path
+        self.mode = mode
+
+        self.fs = fs
+        self.stream = stream
+
+        self.blocksize = self.block_size = block_size
+        self.kwargs = kwargs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return self.close()
+
+
+class HadoopFileSystemWrapper(ArrowFSWrapper):
+
+    protocol = "hdfs"
+
+    def __init__(
+        self,
+        host="default",
+        port=0,
+        user=None,
+        kerb_ticket=None,
+        extra_conf=None,
+        **kwargs,
+    ):
+        from pyarrow.fs import HadoopFileSystem
+
+        fs = HadoopFileSystem(
+            host=host,
+            port=port,
+            user=user,
+            kerb_ticket=kerb_ticket,
+            extra_conf=extra_conf,
+        )
+        super().__init__(fs=fs, **kwargs)
