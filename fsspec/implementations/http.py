9ec95d4468423024ea336a9ec89c0c2491f4d7b9
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-02-18 17:41:37 -0500

    example implementaion for HTTP-streaming

--- fsspec/implementations/http.py
+++ fsspec/implementations/http.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse
 import aiohttp
 import requests
 
-from fsspec.asyn import AsyncFileSystem, sync, sync_wrapper
+from fsspec.asyn import AbstractAsyncFile, AsyncFileSystem, sync, sync_wrapper
 from fsspec.callbacks import _DEFAULT_CALLBACK
 from fsspec.exceptions import FSTimeoutError
 from fsspec.spec import AbstractBufferedFile
@@ -692,6 +692,36 @@ class HTTPStreamFile(AbstractBufferedFile):
 
     def __reduce__(self):
         return reopen, (self.fs, self.url, self.mode, self.blocksize, self.cache.name)
+
+
+class AsyncStreamFile(AbstractAsyncFile):
+    def __init__(self, fs, url, mode="rb", loop=None, session=None, **kwargs):
+        kwargs.pop("asynchronous", False)
+        self.url = url
+        self.loop = loop
+        self.session = session
+        self.r = None
+        if mode != "rb":
+            raise ValueError
+        self.details = {"name": url, "size": None}
+        self.kwargs = kwargs
+        super().__init__(fs=fs, path=url, mode=mode, cache_type="none")
+
+    async def r(self):
+        if self.r is None:
+            r = await self.session.get(self.url, **self.kwargs).__aenter__()
+            self.fs._raise_not_found_for_status(r, self.url)
+            self.r = r
+        return self.r
+
+    async def read(self, num=-1):
+        out = await self.r.content.read(num)
+        self.loc += len(out)
+        return out
+
+    async def close(self):
+        await self.r.close()
+        self.closed = True
 
 
 async def get_range(session, url, start, end, file=None, **kwargs):
