80ab67e43693a9df54c7653f0049113e694e0884
Martin Durant <martindurant@users.noreply.github.com>
2022-03-02 08:49:43 -0500

    Update fsspec/registry.py

--- fsspec/registry.py
+++ fsspec/registry.py
@@ -254,5 +254,8 @@ def filesystem(protocol, **storage_options):
 
 
 def available_protocols():
-    """Return a list of the implemented protocols."""
+    """Return a list of the implemented protocols.
+    
+    Note that any given protocol may require extra packages to be importable.
+    """
     return list(known_implementations)
