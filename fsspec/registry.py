4a7f4ae4f072bec3404190752014b0a708b77574
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-09-20 11:11:04 +0545

    add DVCFileSystem to the registry

--- fsspec/registry.py
+++ fsspec/registry.py
@@ -202,6 +202,10 @@ known_implementations = {
         "class": "webdav4.fsspec.WebdavFileSystem",
         "err": "Install webdav4 to access WebDAV",
     },
+    "dvc": {
+        "class": "dvc.api.DVCFileSystem",
+        "err": "Install dvc to access DVCFileSystem",
+    },
     "root": {
         "class": "fsspec_xrootd.XRootDFileSystem",
         "err": "Install fsspec-xrootd to access xrootd storage system."
