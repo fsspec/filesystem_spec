0010739aa2cc6235632f6597b4a044f7cbb4170f
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-10-03 21:35:59 +0545

    registry: add asynclocalfs to the registry

--- fsspec/registry.py
+++ fsspec/registry.py
@@ -145,6 +145,10 @@ known_implementations = {
         "class": "ocifs.OCIFileSystem",
         "err": "Install ocifs to access OCI Object Storage",
     },
+    "asynclocalfs": {
+        "class": "morefs.asyn_local.AsyncLocalFileSystem",
+        "err": "Install 'morefs[asynclocalfs]' to use AsyncLocalFileSystem",
+    },
     "adl": {
         "class": "adlfs.AzureDatalakeFileSystem",
         "err": "Install adlfs to access Azure Datalake Gen1",
