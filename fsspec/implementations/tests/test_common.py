fe62c94e1763102d9a94c971c5536b0711cf4573
Yuxuan Lu <yuxlu@linkedin.com>
2022-11-07 13:46:32 +0800

    Add assert message

--- fsspec/implementations/tests/test_common.py
+++ fsspec/implementations/tests/test_common.py
@@ -39,12 +39,12 @@ def test_signature(fscls):
     abstract_fs = AbstractFileSystem
     for method in dir(abstract_fs):
         if isfunction(getattr(abstract_fs, method)):
-            print(method)
             if method.startswith('_'): continue
             abs_signature = signature(getattr(abstract_fs, method))
             fs_signature = signature(getattr(fscls, method))
             # assert abs_signature == fs_signature
-            for k in abs_signature.parameters:
-                if k in ['self', 'args', 'kwargs']: continue
-                assert abs_signature.parameters[k] == fs_signature.parameters[k]
+            for k1, k2 in zip(abs_signature.parameters, fs_signature.parameters):
+                if k1 in ['self', 'args', 'kwargs']: continue
+                assert abs_signature.parameters[k1].kind == fs_signature.parameters[k2].kind, f"Paramete {k2} of {method} in {fscls.__name__} don't have the same kind with the one in base class"
+                assert abs_signature.parameters[k1].default == fs_signature.parameters[k2].default, f"Paramete {k2} of {method} in {fscls.__name__} don't have the same default value with the one in base class"
     assert False
