bd45955497a97d90005560950d592da8f8dd3266
Yuxuan Lu <yuxlu@linkedin.com>
2022-11-08 23:42:57 +0800

    Update tests

--- fsspec/implementations/tests/test_common.py
+++ fsspec/implementations/tests/test_common.py
@@ -35,6 +35,72 @@ def test_modified(fs: AbstractFileSystem, temp_file):
 
 
 @pytest.mark.parametrize(
+    "method",
+    [
+ 'cat',
+ 'cat_file',
+ 'cat_ranges',
+ 'checksum',
+ 'copy',
+ 'cp',
+ 'cp_file',
+ 'created',
+ 'delete',
+ 'disk_usage',
+ 'download',
+ 'du',
+ 'end_transaction',
+ 'exists',
+ 'expand_path',
+ 'find',
+ 'from_json',
+ 'get',
+ 'get_file',
+ 'get_mapper',
+ 'glob',
+ 'head',
+ 'info',
+ 'invalidate_cache',
+ 'isdir',
+ 'isfile',
+ 'lexists',
+ 'listdir',
+ 'ls',
+ 'makedir',
+ 'makedirs',
+ 'mkdir',
+ 'mkdirs',
+ 'modified',
+ 'move',
+ 'mv',
+ 'open',
+ 'pipe',
+ 'pipe_file',
+ 'put',
+ 'put_file',
+ 'read_block',
+ 'read_bytes',
+ 'read_text',
+ 'rename',
+ 'rm',
+ 'rm_file',
+ 'rmdir',
+ 'sign',
+ 'size',
+ 'sizes',
+ 'start_transaction',
+ 'stat',
+ 'tail',
+ 'to_json',
+ 'touch',
+ 'ukey',
+ 'unstrip_protocol',
+ 'upload',
+ 'walk',
+ 'write_bytes',
+ 'write_text']
+)
+@pytest.mark.parametrize(
     "fscls",
     [
         "file",
@@ -62,29 +128,24 @@ def test_modified(fs: AbstractFileSystem, temp_file):
     ],
     indirect=["fscls"],
 )
-def test_signature(fscls):
+def test_signature(fscls, method):
     abstract_fs = AbstractFileSystem
-    for method in dir(abstract_fs):
-        if isfunction(getattr(abstract_fs, method)):
-            if method.startswith("_"):
-                continue
-            abs_signature = signature(getattr(abstract_fs, method))
-            fs_signature = signature(getattr(fscls, method))
-            # assert abs_signature == fs_signature
-            for k1, k2 in zip(abs_signature.parameters, fs_signature.parameters):
-                if k1 in ["self", "args", "kwargs"]:
-                    continue
-                assert (
-                    abs_signature.parameters[k1].kind
-                    == fs_signature.parameters[k2].kind
-                ), (
-                    f"Paramete {k2} of {method} in {fscls.__name__} "
-                    "don't have the same kind with the one in base class"
-                )
-                assert (
-                    abs_signature.parameters[k1].default
-                    == fs_signature.parameters[k2].default
-                ), (
-                    f"Paramete {k2} of {method} in {fscls.__name__} don't"
-                    " have the same default value with the one in base class"
-                )
+    abs_signature = signature(getattr(abstract_fs, method))
+    fs_signature = signature(getattr(fscls, method))
+    # We only compare parameters shown in the abstract class
+    # We allow extra parameters in the subclass
+    for k1, k2 in zip(abs_signature.parameters, fs_signature.parameters):
+        if k1 in ["self", "args", "kwargs"]:
+            continue
+        assert (
+            abs_signature.parameters[k1].kind
+            == fs_signature.parameters[k2].kind
+        ), (
+            f"{abs_signature} != {fs_signature}"
+        )
+        assert (
+            abs_signature.parameters[k1].default
+            == fs_signature.parameters[k2].default
+        ), (
+            f"{abs_signature} != {fs_signature}"
+        )
