d8d7ac53fb71fbbe6fa17e85288e79a687f7969f
Martin Durant <martindurant@users.noreply.github.com>
2022-12-08 12:33:35 -0500

    Use tar extractfile() (#1132)
    * Use tar extractfile()
    
    * add timeout to friends env

--- fsspec/implementations/tar.py
+++ fsspec/implementations/tar.py
@@ -1,7 +1,5 @@
-import copy
 import logging
 import tarfile
-from io import BufferedReader
 
 import fsspec
 from fsspec.archive import AbstractArchiveFileSystem
@@ -83,7 +81,7 @@ class TarFileSystem(AbstractArchiveFileSystem):
 
         self._fo_ref = fo
         self.fo = fo  # the whole instance is a context
-        self.tar = tarfile.TarFile(fileobj=self.fo)
+        self.tar: tarfile.TarFile = tarfile.TarFile(fileobj=self.fo)
         self.dir_cache = None
 
         self.index_store = index_store
@@ -122,64 +120,4 @@ class TarFileSystem(AbstractArchiveFileSystem):
         details, offset = self.index[path]
         if details["type"] != "file":
             raise ValueError("Can only handle regular files")
-
-        # `LocalFileSystem` offers its resources as `io.BufferedReader`
-        # objects, those can't be copied.
-        if isinstance(self.fo, BufferedReader):
-            newfo = self.fo
-        else:
-            newfo = copy.copy(self.fo)
-        newfo.seek(offset)
-
-        return TarContainedFile(newfo, self.info(path))
-
-
-class TarContainedFile(object):
-    """
-    Represent/wrap a TarFileSystem's file object.
-    """
-
-    def __init__(self, of, info):
-        self.info = info
-        self.size = info["size"]
-        self.of = of
-        self.start = of.tell()
-        self.end = self.start + self.size
-        self.closed = False
-
-    def tell(self):
-        return self.of.tell() - self.start
-
-    def read(self, n=-1):
-        if self.closed:
-            raise ValueError("file is closed")
-        if n < 0:
-            n = self.end - self.of.tell()
-        if n > self.end - self.tell():
-            n = self.end - self.tell()
-        if n < 1:
-            return b""
-        return self.of.read(n)
-
-    def seek(self, to, whence=0):
-        if self.closed:
-            raise ValueError("file is closed")
-        if whence == 0:
-            to = min(max(self.start, self.start + to), self.end)
-        elif whence == 1:
-            to = min(max(self.start, self.tell() + to), self.end)
-        elif whence == 2:
-            to = min(max(self.start, self.end + to), self.end)
-        else:
-            raise ValueError("Whence must be (0, 1, 2)")
-        self.of.seek(to)
-
-    def close(self):
-        self.of.close()
-        self.closed = True
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
+        return self.tar.extractfile(path)
