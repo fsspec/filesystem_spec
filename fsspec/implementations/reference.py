93315dafc9d40b7cb4cc0b09c4005f57516acde6
Andreas Poehlmann <andreas@poehlmann.io>
2023-01-19 16:41:25 +0100

    fsspec.implemenations.reference: fix data order in no-merging code path

--- fsspec/implementations/reference.py
+++ fsspec/implementations/reference.py
@@ -357,7 +357,8 @@ class ReferenceFileSystem(AsyncFileSystem):
             bytes_out = fs.cat_ranges(new_paths, new_starts, new_ends)
             if len(urls2) == len(bytes_out):
                 # we didn't do any merging
-                for p, b in zip(paths2, bytes_out):
+                for url, b in zip(new_paths, bytes_out):
+                    p = paths2[urls2.index(url)]
                     out[p] = b
             else:
                 # unbundle from merged bytes - simple approach
