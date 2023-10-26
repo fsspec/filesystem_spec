8f1fff4dab430ec0c7b0ecf373561981f51f00c7
Daniel Tsiang <74436899+DanielTsiang@users.noreply.github.com>
2023-10-27 00:31:33 +0100

    Catch PermissionError in fsspec.core when attempting to auto_mkdir
    Catch `PermissionError` in fsspec.core when attempting to auto_mkdir for parent dir, as the user may not have permission to create the parent dir. 
    
    This should hopefully allow the user to carry on without error if the parent dir already exists. If not, a later exception should be thrown when trying to write to a file when the parent dir doesn't exist.
    
    This attempts to address the following issue:
    * https://github.com/fsspec/filesystem_spec/issues/1404
    
    Comments on how to improve this PR are welcome, as this is my first time contributing to the repo.

--- fsspec/core.py
+++ fsspec/core.py
@@ -290,7 +290,11 @@ def open_files(
         fs.auto_mkdir = auto_mkdir
     elif "r" not in mode and auto_mkdir:
         parents = {fs._parent(path) for path in paths}
-        [fs.makedirs(parent, exist_ok=True) for parent in parents]
+        try:
+            [fs.makedirs(parent, exist_ok=True) for parent in parents]
+        except PermissionError:
+            # may not have permission to make parent dir
+            pass
     return OpenFiles(
         [
             OpenFile(
