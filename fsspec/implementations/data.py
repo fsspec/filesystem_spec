c9228cddc5d200ff674b0f18b239e2b23bc5e2d9
Martin Durant <martin.durant@alumni.utoronto.ca>
2023-11-09 15:10:18 -0500

    Doc and info for dataFS

--- fsspec/implementations/data.py
+++ fsspec/implementations/data.py
@@ -18,11 +18,21 @@ class DataFileSystem(AbstractFileSystem):
 
     protocol = "data"
 
+    def __init__(self, **kwargs):
+        """No parameters for this filesystem"""
+        super().__init__(**kwargs)
+
     def cat_file(self, path, start=None, end=None, **kwargs):
         pref, data = path.split(",", 1)
         if pref.endswith("base64"):
             return base64.b64decode(data)[start:end]
         return unquote(data).encode()[start:end]
+
+    def info(self, path, **kwargs):
+        pref, name = path.split(",", 1)
+        data = self.cat_file(path)
+        mime = pref.split(":", 1)[1].split(";", 1)[0]
+        return {"name": name, "size": len(data), "type": "file", "mimetype": mime}
 
     def _open(
         self,
