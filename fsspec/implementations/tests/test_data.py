453d8c5bae2a4cafc7aab9dfb728d1bb5880bbcc
Martin Durant <martindurant@users.noreply.github.com>
2023-11-09 15:27:30 -0500

    Doc and info for dataFS (#1421)

--- fsspec/implementations/tests/test_data.py
+++ fsspec/implementations/tests/test_data.py
@@ -7,3 +7,14 @@ def test_1():
 
     with fsspec.open("data:,Hello%2C%20World%21") as f:
         assert f.read() == b"Hello, World!"
+
+
+def test_info():
+    fs = fsspec.filesystem("data")
+    info = fs.info("data:text/html,%3Ch1%3EHello%2C%20World%21%3C%2Fh1%3E")
+    assert info == {
+        "name": "%3Ch1%3EHello%2C%20World%21%3C%2Fh1%3E",
+        "size": 22,
+        "type": "file",
+        "mimetype": "text/html",
+    }
