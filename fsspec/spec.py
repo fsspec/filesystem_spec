e5f4790ab59bb7605f8722ce3760c0015ca627ce
Nicholas Junge <nicholas.junge@web.de>
2023-12-20 15:02:24 +0100

    Fix non-str case in `AbstractFileSystem.expand_path()`
    If the input to `expand_path` is not a string, it needs to be iterable,
    since the if-branch for the container case tries to loop over the contents.
    
    Previously, the code would fall through to the iterable case even for
    perfectly legal inputs to `AbstractFileSystem._strip_protocol()`, like
    `pathlib.Path`s, causing a crash with a less-than-ideal error message.
    
    All of these inputs are now legalized by a changed `isinstance` check on
    the scalar case, which now contains all types legal in `_strip_protocol`
    and by proxy `stringify_path`.

--- fsspec/spec.py
+++ fsspec/spec.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import io
 import logging
 import os
+import pathlib
 import threading
 import warnings
 import weakref
@@ -1134,7 +1135,7 @@ class AbstractFileSystem(metaclass=_Cached):
         if maxdepth is not None and maxdepth < 1:
             raise ValueError("maxdepth must be at least 1")
 
-        if isinstance(path, str):
+        if isinstance(path, (str, os.PathLike, pathlib.Path)):
             out = self.expand_path([path], recursive, maxdepth)
         else:
             out = set()
