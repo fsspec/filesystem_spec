24060ccc55dfb02e02f6146b502fddf02d7cbc88
skshetry <18718008+skshetry@users.noreply.github.com>
2024-01-10 19:59:50 +0545

    callbacks: introduce scoped callbacks (#1460)

--- fsspec/tests/test_callbacks.py
+++ fsspec/tests/test_callbacks.py
@@ -26,6 +26,41 @@ def test_callbacks_as_callback():
     assert real_callback.call("something", arg=2) == 4
 
 
+def test_callbacks_as_context_manager(mocker):
+    spy_close = mocker.spy(Callback, "close")
+
+    with Callback() as cb:
+        assert isinstance(cb, Callback)
+
+    spy_close.assert_called_once()
+
+
+def test_callbacks_branched():
+    callback = Callback()
+    kwargs = {"key": "value"}
+
+    branch = callback.branched("path_1", "path_2", kwargs)
+
+    assert branch is not callback
+    assert isinstance(branch, Callback)
+    assert kwargs == {"key": "value"}
+
+
+@pytest.mark.asyncio
+async def test_callbacks_branch_coro(mocker):
+    async_fn = mocker.AsyncMock(return_value=10)
+    callback = Callback()
+    wrapped_fn = callback.branch_coro(async_fn)
+    spy = mocker.spy(callback, "branched")
+
+    assert await wrapped_fn("path_1", "path_2", key="value") == 10
+
+    spy.assert_called_once_with("path_1", "path_2", {"key": "value"})
+    async_fn.assert_called_once_with(
+        "path_1", "path_2", callback=spy.spy_return, key="value"
+    )
+
+
 def test_callbacks_wrap():
     events = []
 
