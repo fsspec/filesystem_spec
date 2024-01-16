cd48c8417b6d4dbfa9fcbdfe4043c4409c443f5b
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-01-16 09:33:13 -0800

    Change to check=True for subprocess

--- fsspec/tests/test_fuse.py
+++ fsspec/tests/test_fuse.py
@@ -123,7 +123,7 @@ def test_chmod(mount_local):
         ["cp", str(mount_dir / "text"), str(mount_dir / "new")],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        check=False,
+        check=True,
     )
 
     assert cp.stderr == b""
