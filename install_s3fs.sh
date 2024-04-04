5ec5b00e5255f1478951d7807676ab01025c0770
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-04-03 20:19:16 -0400

    possibly simplofy

--- install_s3fs.sh
+++ install_s3fs.sh
@@ -2,17 +2,12 @@
 # We need to instead do an editible install. Further, s3fs is very specific
 # about the version of fsspec that it installs.
 
-pip uninstall fsspec s3fs -y
-
 rm -r -f downstream
 mkdir ./downstream
 
 # Download source to get tests
-pip download --no-deps --no-binary :all: s3fs==2024.3.1
-tar -xf s3fs-2024.3.1.tar.gz
-mv s3fs-2024.3.1 ./downstream/s3fs
+git clone https://github.com/fsspec/s3fs
+mv s3fs ./downstream/s3fs
 
-pip install --no-deps s3fs
 # s3fs is pinned to a specific version of fsspec
-pip install -e ./downstream/s3fs
-pip install -e .
+pip install -e ./downstream/s3fs # installs all deps, including latest released fsspec
