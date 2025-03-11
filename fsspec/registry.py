d1aaebf630c38611f26cb10253a464789774e0c4
Enric Tobella <enric.tobella@dixmit.com>
2025-03-11 16:27:14 +0100

    Do not force registry err (#1804)

--- fsspec/registry.py
+++ fsspec/registry.py
@@ -248,7 +248,7 @@ def get_filesystem_class(protocol):
         try:
             register_implementation(protocol, _import_class(bit["class"]))
         except ImportError as e:
-            raise ImportError(bit["err"]) from e
+            raise ImportError(bit.get("err")) from e
     cls = registry[protocol]
     if getattr(cls, "protocol", None) in ("abstract", None):
         cls.protocol = protocol
