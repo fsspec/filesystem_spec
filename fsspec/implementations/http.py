4cd61eb08fa8efc335778bc59c996a00c43ba93e
Aleksandar Mastilovic <amastilovic@wikimedia.org>
2025-07-10 15:11:49 -0700

    Fix HTTPFileSystem isdir downloads the whole file issue
    Method _ls_real tries to download the whole r.text() of a link
    regardless of the type of HTML content. Prevent this download in all
    cases except when Content-Type header is not set, or it is set to text/html

--- fsspec/implementations/http.py
+++ fsspec/implementations/http.py
@@ -158,14 +158,20 @@ class HTTPFileSystem(AsyncFileSystem):
         session = await self.set_session()
         async with session.get(self.encode_url(url), **self.kwargs) as r:
             self._raise_not_found_for_status(r, url)
-            try:
-                text = await r.text()
-                if self.simple_links:
-                    links = ex2.findall(text) + [u[2] for u in ex.findall(text)]
-                else:
-                    links = [u[2] for u in ex.findall(text)]
-            except UnicodeDecodeError:
-                links = []  # binary, not HTML
+            url_info = await self._info(url, **kwargs)
+            mimetype = url_info.get("mimetype", None)
+            if mimetype in ("text/html", None):
+                try:
+                    text = await r.text()
+                    if self.simple_links:
+                        links = ex2.findall(text) + [u[2] for u in ex.findall(text)]
+                    else:
+                        links = [u[2] for u in ex.findall(text)]
+                except UnicodeDecodeError:
+                    links = []  # binary, not HTML
+            else:
+                links = []
+
         out = set()
         parts = urlparse(url)
         for l in links:
