72698409ce703292cc4847af3d557785b326e194
Martin Durant <martin.durant@alumni.utoronto.ca>
2025-09-15 10:50:27 -0400

    reform

--- fsspec/implementations/tests/test_gist.py
+++ fsspec/implementations/tests/test_gist.py
@@ -5,7 +5,7 @@ import pytest
 import fsspec
 from fsspec.implementations.gist import GistFileSystem
 
-if (3, 13) < sys.version_info < (3, 14):
+if sys.version_info[:2] != (3, 12):
     pytest.skip("Too many tests bust rate limit", allow_module_level=True)
 
 
