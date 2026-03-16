94ad3e920cfef00bb82c7d963f66ed89b07e2dc5
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-03-16 21:24:23 +0100

    Apply ruff rule RUF007
    RUF007 Prefer `itertools.pairwise()` over `zip()` when iterating over successive pairs

--- fsspec/implementations/reference.py
+++ fsspec/implementations/reference.py
@@ -1110,7 +1110,7 @@ class ReferenceFileSystem(AsyncFileSystem):
                 subdirs.append(par0)
 
             subdirs.reverse()
-            for parent, child in zip(subdirs, subdirs[1:]):
+            for parent, child in itertools.pairwise(subdirs):
                 # register newly discovered directories
                 assert child not in self.dircache
                 assert parent in self.dircache
