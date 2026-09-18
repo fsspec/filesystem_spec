51e497460550f34b8a3d47a77ba8be0c22ea29a0
Prince Kumar <princer@google.com>
2026-09-18 06:56:33 +0000

    remove NoOpDirCache class

--- fsspec/dircache.py
+++ fsspec/dircache.py
@@ -21,19 +21,8 @@ class DirCache(MutableMapping):
         }
 
     Parameters to this class control listing expiry or indeed turn
-    caching off
+    caching off.
     """
-
-    def __new__(
-        cls,
-        use_listings_cache=True,
-        listings_expiry_time=None,
-        max_paths=None,
-        **kwargs,
-    ):
-        if not use_listings_cache:
-            return super().__new__(NullDirCache)
-        return super().__new__(cls)
 
     def __init__(
         self,
@@ -42,20 +31,36 @@ class DirCache(MutableMapping):
         max_paths=None,
         **kwargs,
     ):
-        self._cache = OrderedDict() if max_paths else {}
+        """
+        Parameters
+        ----------
+        use_listings_cache: bool
+            If False, this cache never returns items, but always reports KeyError,
+            and setting items has no effect.
+        listings_expiry_time: int or float (optional)
+            Time in seconds that a listing is considered valid. If None,
+            listings do not expire.
+        max_paths: int (optional)
+            The number of most recent listings that are considered valid; 'recent'
+            refers to when the entry was set or accessed.
+        """
+        self._cache = OrderedDict() if max_paths is not None else {}
         self._times = {}
         self.use_listings_cache = use_listings_cache
         self.listings_expiry_time = listings_expiry_time
         self.max_paths = max_paths
 
     def __getitem__(self, item):
-        if self.listings_expiry_time is not None:
-            if self._times.get(item, 0) - time.time() < -self.listings_expiry_time:
+        if not self.use_listings_cache:
+            raise KeyError(item)
+
+        if self.listings_expiry_time is not None and item in self._cache:
+            if time.time() - self._times.get(item, 0) > self.listings_expiry_time:
                 del self[item]
                 raise KeyError(item)
 
-        val = self._cache[item]  # maybe raises KeyError
-        if self.max_paths:
+        val = self._cache[item]
+        if self.max_paths is not None:
             self._cache.move_to_end(item)
         return val
 
@@ -67,18 +72,25 @@ class DirCache(MutableMapping):
         return len(self._cache)
 
     def __contains__(self, item):
+        if not self.use_listings_cache or item not in self._cache:
+            return False
+
         if self.listings_expiry_time is not None:
-            if self._times.get(item, 0) - time.time() < -self.listings_expiry_time:
+            if time.time() - self._times.get(item, 0) > self.listings_expiry_time:
                 del self[item]
                 return False
-        return item in self._cache
+
+        return True
 
     def __setitem__(self, key, value):
+        if not self.use_listings_cache:
+            return
+
         self._cache[key] = value
         if self.listings_expiry_time is not None:
             self._times[key] = time.time()
 
-        if self.max_paths:
+        if self.max_paths is not None:
             self._cache.move_to_end(key)
             if len(self._cache) > self.max_paths:
                 oldest, _ = self._cache.popitem(last=False)
@@ -89,11 +101,16 @@ class DirCache(MutableMapping):
         self._times.pop(key, None)
 
     def __iter__(self):
+        if not self.use_listings_cache:
+            return
+
+        if self.listings_expiry_time is None:
+            yield from self._cache
+            return
+
         now = time.time()
         for key in list(self._cache):
-            if self.listings_expiry_time is not None and (
-                self._times.get(key, 0) - now < -self.listings_expiry_time
-            ):
+            if now - self._times.get(key, 0) > self.listings_expiry_time:
                 del self[key]
             else:
                 yield key
@@ -103,42 +120,3 @@ class DirCache(MutableMapping):
             DirCache,
             (self.use_listings_cache, self.listings_expiry_time, self.max_paths),
         )
-
-
-class NullDirCache(DirCache):
-    """No-op directory listing cache used when use_listings_cache=False"""
-
-    def __init__(
-        self,
-        use_listings_cache=False,
-        listings_expiry_time=None,
-        max_paths=None,
-        **kwargs,
-    ):
-        super().__init__(
-            use_listings_cache=False,
-            listings_expiry_time=listings_expiry_time,
-            max_paths=max_paths,
-            **kwargs,
-        )
-
-    def __getitem__(self, item):
-        raise KeyError(item)
-
-    def __setitem__(self, key, value):
-        pass
-
-    def __delitem__(self, key):
-        pass
-
-    def __contains__(self, item):
-        return False
-
-    def __len__(self):
-        return 0
-
-    def __iter__(self):
-        return iter(())
-
-    def clear(self):
-        pass
--- fsspec/tests/test_dircache.py
+++ fsspec/tests/test_dircache.py
@@ -1,4 +1,6 @@
+import pickle
 import time
+from collections import OrderedDict
 
 import pytest
 
@@ -34,6 +36,13 @@ def test_dircache_expiry():
         _ = dc["x"]
 
 
+def test_dircache_missing_key_with_expiry():
+    dc = DirCache(listings_expiry_time=10)
+    assert "nonexistent" not in dc
+    with pytest.raises(KeyError):
+        _ = dc["nonexistent"]
+
+
 def test_dircache_iter():
     dc = DirCache(max_paths=2, listings_expiry_time=0.1)
     dc["m"] = 1
@@ -46,12 +55,9 @@ def test_dircache_iter():
     assert len(dc) == 0
 
 
-def test_nulldircache():
-    from fsspec.dircache import DirCache, NullDirCache
-
+def test_dircache_disabled():
     dc = DirCache(use_listings_cache=False)
     assert isinstance(dc, DirCache)
-    assert isinstance(dc, NullDirCache)
 
     dc["a"] = 1
     assert "a" not in dc
@@ -62,8 +68,6 @@ def test_nulldircache():
 
 
 def test_dircache_no_max_paths():
-    from collections import OrderedDict
-
     dc_no_max = DirCache(max_paths=None)
     assert type(dc_no_max._cache) is dict
 
@@ -76,3 +80,34 @@ def test_dircache_no_max_paths():
     assert dc_no_max["b"] == 2
     assert len(dc_no_max) == 2
     assert list(dc_no_max) == ["a", "b"]
+
+
+def test_dircache_max_paths_zero():
+    dc = DirCache(max_paths=0)
+    assert type(dc._cache) is OrderedDict
+    dc["a"] = 1
+    assert len(dc) == 0
+    assert "a" not in dc
+
+
+def test_dircache_clear_and_del():
+    dc = DirCache(listings_expiry_time=60, max_paths=10)
+    dc["a"] = 1
+    dc["b"] = 2
+    assert "a" in dc._times
+    del dc["a"]
+    assert "a" not in dc
+    assert "a" not in dc._times
+
+    dc.clear()
+    assert len(dc) == 0
+    assert len(dc._times) == 0
+
+
+def test_dircache_pickle():
+    dc = DirCache(use_listings_cache=True, listings_expiry_time=10, max_paths=5)
+    data = pickle.dumps(dc)
+    dc2 = pickle.loads(data)
+    assert dc2.use_listings_cache is True
+    assert dc2.listings_expiry_time == 10
+    assert dc2.max_paths == 5
